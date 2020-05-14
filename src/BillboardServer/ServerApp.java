9e3b47fd9926a3f5bf0467d0e4310b05b9b0dc85
lachlansutherland <lachlan.sutherland2@gmail.com>
2020-05-14 14:55:41 +1000

    basic test commit

---
+++ src/BillboardServer/ServerApp.java
@@ -0,0 +1,13 @@
+package BillboardServer;
+import java.util.Scanner;
+
+public class ServerApp {
+
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+
+    private static void readProps(){
+
+    }
+}
