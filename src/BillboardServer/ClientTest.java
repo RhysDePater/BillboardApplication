addb73314029bd6e8886f293065dc5e6d73cbafe
Brandon <n10235329@qut.edu.au>
2020-05-20 10:08:49 +1000

    Added a class (ClientSQLCommandFunction.java) to group SQL commands into separate methods to be called in other clients. Added method for deleteUser. Added method for createBillboard. Added method for deleteBillboard. Added method for getBillboard. Added method for login.

--- src/BillboardServer/ClientTest.java
+++ src/BillboardServer/ClientTest.java
@@ -7,48 +7,14 @@ import java.time.LocalDateTime;
 
 public class ClientTest {
     public static void main(String[] args) throws IOException {
-        LocalDateTime startDate = LocalDateTime.of(2015, 2, 20, 6, 30);;
-        //the host and port here should be read from the network.props file.
-        Socket socket = new Socket (ReadFromNetworkPropsFile.readNetworkPropsHost(), ReadFromNetworkPropsFile.readNetworkPropsPort());
-        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-        //BufferedOutputStream bos = new BufferedOutputStream(MyOutputSteam);
-        //oos.writeUTF("add user");
-        //String[] string_array = {"createUser", "testusername", "testpassword", "1", "1", "1", "1"};
-        //String[] string_array = {"deleteUser", "testusername"};
-        //String[] string_array = {"addSchedule", "billboardname", "2015-02-20T06:30", "120", "sessiontoken"};
-        //String[] string_array = {"addSchedule", "billboardname", startDate.toString(), "120", "sessiontoken"};
-        //String[] string_array = {"login", "admin", "p1ass"};
-        String xmlString = XMLStringCreator.createXmlString(); // Here, a string is created from an xml file so that it can be sent to and stored in the database.
-        //String[] string_array = {"createBillboard", "BillboardName", xmlString}; // Command, billboard_name, xml_data
-        //String[] string_array = {"deleteBillboard", "billboardname"};
-        //String[] string_array = {"getBillboard", "billboardname"};
-        String[] string_array = {"deleteSchedule", "billboardname", startDate.toString()};
-        oos.writeObject(string_array);
-        oos.flush();
-        //oos.close();
-        //ois.close();
-        //socket.close();
-        for (;;){ // Now listen for a response
-            Object Response = null;
-            String[] StringArray;
-            try{
-                Response = ois.readObject();
-            }
-            catch(Exception e){
-                System.out.println(e.getMessage());
-            }
-            try{
-                StringArray = (String[])Response;
-            }
-            catch (Exception e ){
-                System.out.println(e.getMessage());
-                return;
-            }
-            System.out.println(StringArray[0]);
-            System.out.println(StringArray[1]);
-            System.out.println(StringArray[2]);
-            //System.out.println(StringArray[0]);
+        // Note: can only run one ClientSQLCommandFunction method at a time at the moment. All currently listed examples function.
+        String xmlString = XMLStringCreator.createXmlString();
+        ClientSQLCommandFunction.RunCommandDeleteUser("testusername");
+        //ClientSQLCommandFunction.RunCommandCreateBillboard("BillboardMethodTest6", xmlString);
+        //ClientSQLCommandFunction.RunCommandDeleteBillboard("BillboardMethodTest2");
+        //ClientSQLCommandFunction.RunCommandGetBillboard("BillboardMethodTest6");
+        //ClientSQLCommandFunction.RunCommandLogin("ADMIN", "pass");
+
         }
     }
-}
+
