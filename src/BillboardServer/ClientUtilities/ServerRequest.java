b1ad60850c4313cd80aa1a097a07133051bf03d9
lachlansutherland <lachlan.sutherland2@gmail.com>
2020-05-30 17:51:28 +1000

    Small change to so that the functions integrate easier to the main branch. They no longer throw exceptions.

--- src/BillboardServer/ClientUtilities/ServerRequest.java
+++ src/BillboardServer/ClientUtilities/ServerRequest.java
@@ -27,66 +27,71 @@ public class ServerRequest {
      *      "A string of results" Not many functions use this currently, getBillboard does as an example, it would return the xml string from the database. For a function like delete user, this will just be "".
      *      "Optional message that could be displayed to the user/debugging purposes", Just print this to the console for greater verbosity
      * See the SendBackData function on the server for more the code responsible
-     *
-     * @throws IOException This is thrown from lots of the networking functions, probably going to happen if the server isn't up, or network props file doesn't exist and a weird socket is made
      */
-    public static String[] sendQuery(String[] queryArray) throws IOException{
-        Socket socket = new Socket (ReadNetworkProps.getHost(), ReadNetworkProps.getPort());
-        socket.setSoTimeout(4000); // Set a two second timeout on read operations. After two seconds of nothing being read, an exception will be thrown
-        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-        oos.writeObject(queryArray);
-        oos.flush();
-        // Now listen for a response, no loop is required because there will only be a single response from the server for now, then return the results
-        Object Response = null;
-        try{
-            Response = ois.readObject();
-        }
-        catch(Exception e){
-            System.out.println(e.getMessage());
-            return null;
-        }
-        try{
-            return (String[])Response;
-        }
-        catch (Exception e ){
-            System.out.println(e.getMessage());
-            return null;
+    public static String[] sendQuery(String[] queryArray){
+        try {
+            Socket socket = new Socket (ReadNetworkProps.getHost(), ReadNetworkProps.getPort());
+            socket.setSoTimeout(4000); // Set a two second timeout on read operations. After two seconds of nothing being read, an exception will be thrown
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            oos.writeObject(queryArray);
+            oos.flush();
+            // Now listen for a response, no loop is required because there will only be a single response from the server for now, then return the results
+            Object Response = null;
+            try{
+                Response = ois.readObject();
+            }
+            catch(Exception e){
+                System.out.println(e.getMessage());
+                return null;
+            }
+            try{
+                return (String[])Response;
+            }
+            catch (Exception e ){
+                System.out.println(e.getMessage());
+                return null;
+            }
+        } catch (IOException e){ //This is caught from lots of the networking functions, probably going to happen if the server isn't up, or network props file doesn't exist and a weird socket is made
+            e.printStackTrace();
+            return  null;
         }
     }
 
     // Same as above but returns a string[][] for use in certain functions, like list users
     // result[0] will be the same as the normal return above, all extra indices will be the results
-    public static String[][] sendQueryAlt(String[] queryArray) throws IOException{
-        Socket socket = new Socket (ReadNetworkProps.getHost(), ReadNetworkProps.getPort());
-        socket.setSoTimeout(4000); // Set a two second timeout on read operations. After two seconds of nothing being read, an exception will be thrown
-        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-        oos.writeObject(queryArray);
-        oos.flush();
-        // Now listen for a response, no loop is required because there will only be a single response from the server for now, then return the results
-        Object Response = null;
-        try{
-            Response = ois.readObject();
-        }
-        catch(Exception e){
-            System.out.println(e.getMessage());
-            return null;
-        }
-        try{ // The returned object from the server will be a double string array if the command was executed successfully
-            return (String[][])Response;
-        }
-        catch (Exception e ){
-            //System.out.println(e.getMessage());
-        }
-        try{ // The returned object from the server will be a single array if the command failed, so it needs to be formatted properly to be returned to the client
-            String[] returnedErrorArray =  (String[])Response;
-            String[] emptyArray = new String[]{""};
-            return new String[][]{returnedErrorArray,emptyArray};
-        }
-        catch (Exception e ){
-            System.out.println(e.getMessage());
-            return null;
+    public static String[][] sendQueryAlt(String[] queryArray){
+        try {
+            Socket socket = new Socket(ReadNetworkProps.getHost(), ReadNetworkProps.getPort());
+            socket.setSoTimeout(4000); // Set a two second timeout on read operations. After two seconds of nothing being read, an exception will be thrown
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            oos.writeObject(queryArray);
+            oos.flush();
+            // Now listen for a response, no loop is required because there will only be a single response from the server for now, then return the results
+            Object Response = null;
+            try {
+                Response = ois.readObject();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                return null;
+            }
+            try { // The returned object from the server will be a double string array if the command was executed successfully
+                return (String[][]) Response;
+            } catch (Exception e) {
+                //System.out.println(e.getMessage());
+            }
+            try { // The returned object from the server will be a single array if the command failed, so it needs to be formatted properly to be returned to the client
+                String[] returnedErrorArray = (String[]) Response;
+                String[] emptyArray = new String[]{""};
+                return new String[][]{returnedErrorArray, emptyArray};
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                return null;
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+            return  null;
         }
     }
 
@@ -105,7 +110,7 @@ public class ServerRequest {
      * @param sessionToken A session token so the server can authenticate the request
      * @return See ServerRequest.sendQuery
      */
-    public static String[] createUser(String username, String password, Integer create_billboard, Integer edit_billboard, Integer schedule_billboard, Integer edit_user, String sessionToken) throws IOException {
+    public static String[] createUser(String username, String password, Integer create_billboard, Integer edit_billboard, Integer schedule_billboard, Integer edit_user, String sessionToken){
         String[] command = {"createUser", username, password, create_billboard.toString(), edit_billboard.toString(), schedule_billboard.toString(), edit_user.toString(), sessionToken};
         return sendQuery(command);
     }
@@ -116,7 +121,7 @@ public class ServerRequest {
      * @param sessionToken A session token so the server can authenticate the request
      * @return See ServerRequest.sendQuery
      */
-    public static String[]  deleteUser(String username, String sessionToken) throws IOException {
+    public static String[]  deleteUser(String username, String sessionToken) {
         String[] command = {"deleteUser", username, sessionToken};
         return sendQuery(command);
     }
@@ -126,7 +131,7 @@ public class ServerRequest {
      * @param sessionToken A session token so the server can authenticate the request
      * @return See ServerRequest.sendQueryDoubleArray
      */
-    public static String[][]  listUsers(String sessionToken) throws IOException {
+    public static String[][]  listUsers(String sessionToken) {
         String[] command = {"listUsers", sessionToken};
         return sendQueryAlt(command);
     }
@@ -137,7 +142,7 @@ public class ServerRequest {
      * @param password The password, which has to match the stored password (only plaintext right now)
      * @return See ServerRequest.sendQuery
      */
-    public static String[]  login(String username, String password) throws IOException {
+    public static String[]  login(String username, String password) {
         String[] command = {"login", username, password};
         return sendQuery(command);
     }
@@ -147,7 +152,7 @@ public class ServerRequest {
      * @param sessionToken The currently logged in users' session token.
      * @return See ServerRequest.sendQuery
      */
-    public static String[] logout(String sessionToken) throws IOException{
+    public static String[] logout(String sessionToken) {
         String[] command = {"logout", sessionToken};
         return sendQuery(command);
     }
@@ -161,12 +166,12 @@ public class ServerRequest {
      * @param edit_user 0 or 1 for disabled or enabled
      * @return See ServerRequest.sendQuery
      */
-    public static String[]  editAllPermissions(String username, Integer create_billboard, Integer edit_billboard, Integer schedule_billboard, Integer edit_user, String sessionToken) throws IOException {
+    public static String[]  editAllPermissions(String username, Integer create_billboard, Integer edit_billboard, Integer schedule_billboard, Integer edit_user, String sessionToken) {
         String[] command = {"editPermission", username, create_billboard.toString(), edit_billboard.toString(), schedule_billboard.toString(), edit_user.toString(), sessionToken};
         return sendQuery(command);
     }
 
-    public static String[][] getPermissions(String username, String sessionToken) throws IOException {
+    public static String[][] getPermissions(String username, String sessionToken) {
         String[] command = {"getPermissions", username, sessionToken};
         return sendQueryAlt(command);
     }
@@ -177,7 +182,7 @@ public class ServerRequest {
      * @param permission Either create_billboard, edit_billboard, schedule_billboard, or edit_user
      * @param value 1 or 0
      */
-    public static String[] editPermission(String username, String permission, Integer value, String sessionToken) throws IOException{
+    public static String[] editPermission(String username, String permission, Integer value, String sessionToken) {
         switch (permission) {
             case "create_billboard":
                 return editAllPermissions(username, value, -1, -1, -1, sessionToken); // -1 is so the value doesn't change on the server side
@@ -195,7 +200,7 @@ public class ServerRequest {
         return new String[]{"false", "", "column " + permission + " doesn't exist"};
     }
 
-    public static String[] setUserPassword(String username, String password, String sessionToken) throws IOException {
+    public static String[] setUserPassword(String username, String password, String sessionToken) {
         String[] command = {"setUserPassword", username, password, sessionToken};
         return sendQuery(command);
     }
@@ -211,7 +216,7 @@ public class ServerRequest {
      * @param sessionToken A session token so the server can authenticate the request
      * @return See ServerRequest.sendQuery
      */
-    public static String[]  createOrEditBillboard(String billboardName, String xml_string, String sessionToken) throws IOException {
+    public static String[]  createOrEditBillboard(String billboardName, String xml_string, String sessionToken) {
         String[] command = {"createBillboard", billboardName, xml_string, sessionToken};
         return sendQuery(command);
     }
@@ -223,7 +228,7 @@ public class ServerRequest {
      * @param sessionToken A session token so the server can authenticate the request
      * @return See ServerRequest.sendQuery
      */
-    public static String[]  getBillboard(String billboardName, String sessionToken) throws IOException {
+    public static String[]  getBillboard(String billboardName, String sessionToken) {
         String[] command = {"getBillboard", billboardName, sessionToken};
         return sendQuery(command);
     }
@@ -234,7 +239,7 @@ public class ServerRequest {
      * @param sessionToken A session token so the server can authenticate the request
      * @return See ServerRequest.sendQuery
      */
-    public static String[]  deleteBillboard(String billboardName, String sessionToken) throws IOException {
+    public static String[]  deleteBillboard(String billboardName, String sessionToken) {
         String[] command = {"deleteBillboard", billboardName, sessionToken};
         return sendQuery(command);
     }
@@ -244,7 +249,7 @@ public class ServerRequest {
      * @param sessionToken A session token so the server can authenticate the request
      * @return See ServerRequest.sendQueryDoubleArray
      */
-    public static String[][]  listBillboards(String sessionToken) throws IOException {
+    public static String[][]  listBillboards(String sessionToken) {
         String[] command = {"listBillboards", sessionToken};
         return sendQueryAlt(command);
     }
@@ -253,7 +258,7 @@ public class ServerRequest {
      * Get the contents of the currently scheduled billboard, or a placeholder
      * @return See ServerRequest.sendQueryDoubleArray
      */
-    public static String[]  getCurrentBillboard() throws IOException {
+    public static String[]  getCurrentBillboard() {
         String[] command = {"getCurrentBillboard"};
         return sendQuery(command);
     }
@@ -270,7 +275,7 @@ public class ServerRequest {
      * @param sessionToken A session token so the server can authenticate the request
      * @return See ServerRequest.sendQuery
      */
-    public static String[]  addSchedule(String billboardName, LocalDateTime startTime, Integer duration, String sessionToken) throws IOException {
+    public static String[]  addSchedule(String billboardName, LocalDateTime startTime, Integer duration, String sessionToken) {
         String[] command = {"addSchedule", billboardName, startTime.toString(), duration.toString(), sessionToken};
         return sendQuery(command);
     }
@@ -284,7 +289,7 @@ public class ServerRequest {
      * @param sessionToken A session token so the server can authenticate the request
      * @return See ServerRequest.sendQuery
      */
-    public static String[]  deleteSchedule(String billboardName, LocalDateTime startTime, String sessionToken) throws IOException {
+    public static String[]  deleteSchedule(String billboardName, LocalDateTime startTime, String sessionToken) {
         String[] command = {"deleteSchedule", billboardName, startTime.toString(), sessionToken};
         return sendQuery(command);
     }
@@ -294,7 +299,7 @@ public class ServerRequest {
      * @param sessionToken A session token so the server can authenticate the request
      * @return See ServerRequest.sendQueryDoubleArray
      */
-    public static String[][]  listSchedules(String sessionToken) throws IOException {
+    public static String[][]  listSchedules(String sessionToken) {
         String[] command = {"listSchedules", sessionToken};
         return sendQueryAlt(command);
     }
@@ -303,60 +308,10 @@ public class ServerRequest {
     // OTHER
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public static String[][] getColumns(String table_name, String sessionToken) throws IOException {
+    public static String[][] getColumnNames(String table_name, String sessionToken) {
         String[] command = {"getColumns", table_name, sessionToken};
         return sendQueryAlt(command);
     }
 
 }
-
-
-// Just ignore all this, it's from testing
-/*
-    public static void main(String[] args) throws IOException {
-        LocalDateTime startDate = LocalDateTime.of(2015, 2, 20, 6, 30);;
-        //the host and port here should be read from the network.props file.
-        Socket socket = new Socket (ReadNetworkProps.readNetworkPropsHost(), ReadNetworkProps.readNetworkPropsPort());
-        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-        //BufferedOutputStream bos = new BufferedOutputStream(MyOutputSteam);
-        //oos.writeUTF("add user");
-        String[] string_array = {"createUser", "testusername", "testpassword", "1", "1", "1", "1"};
-        //String[] string_array = {"deleteUser", "testusername"};
-        //String[] string_array = {"addSchedule", "billboardname", "2015-02-20T06:30", "120", "sessiontoken"};
-        //String[] string_array = {"addSchedule", "billboardname", startDate.toString(), "120", "sessiontoken"};
-        //String[] string_array = {"login", "admin", "p1ass"};
-        String xmlString = XMLStringCreator.createXmlString(); // Here, a string is created from an xml file so that it can be sent to and stored in the database.
-        //String[] string_array = {"createBillboard", "BillboardName", xmlString}; // Command, billboard_name, xml_data
-        //String[] string_array = {"deleteBillboard", "billboardname"};
-        //String[] string_array = {"getBillboard", "billboardname"};
-        //String[] string_array = {"deleteSchedule", "billboardname", startDate.toString()};
-        oos.writeObject(string_array);
-        oos.flush();
-        //oos.close();
-        //ois.close();
-        //socket.close();
-        for (;;){ // Now listen for a response
-            Object Response = null;
-            String[] StringArray;
-            try{
-                Response = ois.readObject();
-            }
-            catch(Exception e){
-                System.out.println(e.getMessage());
-            }
-            try{
-                StringArray = (String[])Response;
-            }
-            catch (Exception e ){
-                System.out.println(e.getMessage());
-                return;
-            }
-            System.out.println(StringArray[0]);
-            System.out.println(StringArray[1]);
-            System.out.println(StringArray[2]);
-            //System.out.println(StringArray[0]);
-        }
-    }
-*/
 
